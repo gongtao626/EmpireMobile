f19a0e92cb77d16ad6921732a0a22faa0648ce4e
Tao Gong <gongtaol@gmail.com>
2018-06-19 11:17:16 +1000

    Continuous Ignore and action tool button
    1 Ignore the continuous by set interval using java.getcurrentmillions
    2 action bar using menu action for camera and light
    3 delete the camera and light button

--- EmpireCouriers/app/src/main/java/com/example/liaojingxi/empirecouriers/DriverLoginActivity.java
+++ EmpireCouriers/app/src/main/java/com/example/liaojingxi/empirecouriers/DriverLoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.liaojingxi.empirecouriers;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
@@ -44,43 +50,49 @@ public class DriverLoginActivity extends AppCompatActivity {
     //TO add
     private boolean isTorchOn = false;
 
+    private static final int DELAY = 1000; // 1 second
+
     private BarcodeCallback callback = new BarcodeCallback() {
+        private long lastTimestamp = 0;
+
         @Override
         public void barcodeResult(BarcodeResult result) {
-            if(result.getText() == null || result.getText().equals(lastText)) {
-                // Prevent duplicate scans
-                return;
+            if (result.getText() != null) {
+                if(System.currentTimeMillis() - lastTimestamp < DELAY) {
+                    // Too soon after the last barcode - ignore.
+                    return;
+                }
+                lastTimestamp = System.currentTimeMillis();
+
+                lastText = result.getText();
+
+                barcodeView.setStatusText(result.getText());
+
+
+                beepManager.setVibrateEnabled(true);
+                //beepManager.setBeepEnabled(false);
+                beepManager.playBeepSoundAndVibrate();
+
+                String scriptForBlur = "document.getElementsByTagName('input')[0].blur();";
+
+                //String script = "var inputEle = document.getElementsByTagName('input')[0];var event1 = document.createEvent('HTMLEvents');  event1.initEvent('change', true, true); event1.eventType = 'change'; inputEle.dispatchEvent(event1);";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    browserTest.evaluateJavascript(scriptForBlur, null);
+                } else {
+                    browserTest.loadUrl("javascript:" + scriptForBlur);
+                }
+
+                String script = "var inputEle = document.getElementsByTagName('input')[0];inputEle.value = '" + lastText + "';var event1 = document.createEvent('HTMLEvents');  event1.initEvent('change', true, true); event1.eventType = 'change'; inputEle.dispatchEvent(event1);";
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    browserTest.evaluateJavascript(script, null);
+                } else {
+                    browserTest.loadUrl("javascript:" + script);
+                }
+                //Added preview of scanned barcode
+                //ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+                //imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
             }
-
-            lastText = result.getText();
-
-            barcodeView.setStatusText(result.getText());
-
-
-            beepManager.setVibrateEnabled(true);
-            //beepManager.setBeepEnabled(false);
-            beepManager.playBeepSoundAndVibrate();
-
-            String scriptForBlur = "document.getElementsByTagName('input')[0].blur();";
-
-            //String script = "var inputEle = document.getElementsByTagName('input')[0];var event1 = document.createEvent('HTMLEvents');  event1.initEvent('change', true, true); event1.eventType = 'change'; inputEle.dispatchEvent(event1);";
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                browserTest.evaluateJavascript(scriptForBlur, null);
-            } else {
-                browserTest.loadUrl("javascript:" + scriptForBlur);
-            }
-
-            String script = "var inputEle = document.getElementsByTagName('input')[0];inputEle.value = '"+lastText+"';var event1 = document.createEvent('HTMLEvents');  event1.initEvent('change', true, true); event1.eventType = 'change'; inputEle.dispatchEvent(event1);";
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                browserTest.evaluateJavascript(script, null);
-            } else {
-                browserTest.loadUrl("javascript:" + script);
-            }
-            //Added preview of scanned barcode
-            //ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
-            //imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
         }
-
         @Override
         public void possibleResultPoints(List<ResultPoint> resultPoints) {
         }
@@ -111,25 +123,11 @@ public class DriverLoginActivity extends AppCompatActivity {
         //TO add
 
         browserTest = (WebView) findViewById(R.id.driverLoginAct2);
-        //browserTest.setFocusable(false);
-        //browserTest.setFocusableInTouchMode(false);
 
         final WebSettings webSettings = browserTest.getSettings();
-
         webSettings.setJavaScriptEnabled(true);
-        //CookieManager.getInstance().setAcceptCookie(true);
-
-
-        //webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        //webSettings.setSaveFormData(true);
-        //webSettings.setDatabaseEnabled(true);
-
-
         webSettings.setDomStorageEnabled(true);
         webSettings.setAppCacheEnabled(true);
-        //if (Build.VERSION.SDK_INT >= 21) {
-        //webSettings.setAllowContentAccess(true);
-        //}
 
 
         browserTest.setWebViewClient(new WebViewClient() {
@@ -177,7 +175,7 @@ public class DriverLoginActivity extends AppCompatActivity {
 
                     view.loadUrl("https://empirecouriers.willowit.net.au:8443/web/driverlogin");
                 }else if(url.contains("https://empirecouriers.willowit.net.au:8443/driver/web?#action=driver.ui")){
-                    camera.setVisibility(View.VISIBLE);
+                   // camera.setVisibility(View.VISIBLE);
 
                     if (progressBar.isShowing()) {
                         progressBar.dismiss();
@@ -189,19 +187,47 @@ public class DriverLoginActivity extends AppCompatActivity {
                 //}
             }
         });
+        //if(isNetworkAvailable()) {
+          //  progressBar = ProgressDialog.show(DriverLoginActivity.this, "", "Loading...");
+        //} //xin dai ma
         progressBar = ProgressDialog.show(DriverLoginActivity.this, "", "Loading...");
         browserTest.loadUrl("https://empirecouriers.willowit.net.au:8443/driver/web?");
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                receivedLabel_menu();
+                return true;
+            case R.id.action_light:
+                torchOnOff_menu();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuaction, menu);
+        return true;
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();//Add permission of ACCESS_NETWORK_STATE
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }//xin dai ma
 
     public void receivedLabel2(View view) {
         Log.d("test", "Scan button Starts to work!");
 
-        //barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
-        //barcodeView.setVisibility(View.GONE);
-        //Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39, BarcodeFormat.UPC_A, BarcodeFormat.EAN_13);
-
-        //barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
         if (barcodeView.getVisibility() == barcodeView.VISIBLE) {
 
             //TO add
@@ -211,8 +237,6 @@ public class DriverLoginActivity extends AppCompatActivity {
 
             light.setVisibility(View.GONE);
 
-            //lastText = "";//To allow scan again same label
-            //browserTest.setLayoutParams(WebView.LayoutParams(ViewGroup.LayoutParams.));
         } else {
 
             if(isTorchOn == true) {
@@ -288,4 +312,66 @@ public class DriverLoginActivity extends AppCompatActivity {
 
         return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
+
+    private void receivedLabel_menu() {
+        Log.d("test", "Scan button Starts to work!");
+
+        //barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+        //barcodeView.setVisibility(View.GONE);
+        //Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39, BarcodeFormat.UPC_A, BarcodeFormat.EAN_13);
+
+        //barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(formats));
+        if (barcodeView.getVisibility() == barcodeView.VISIBLE) {
+
+            //TO add
+            barcodeView.pause();
+
+            barcodeView.setVisibility(View.GONE);
+            //barcodeView.stopDecoding();
+            //light.setVisibility(View.GONE);
+
+            //lastText = "";//To allow scan again same label
+            //browserTest.setLayoutParams(WebView.LayoutParams(ViewGroup.LayoutParams.));
+        } else {
+
+            if (isTorchOn == true) {
+                barcodeView.setTorchOff();
+                isTorchOn = false;
+            }
+            //light.setVisibility(View.VISIBLE);
+
+            lastText = "";//To allow scan again same label
+            //TO add
+            barcodeView.resume();
+
+            //browserTest.setLayoutParams(new ViewGroup.LayoutParams(200, 400));
+
+            barcodeView.setVisibility(View.VISIBLE);
+
+            barcodeView.decodeContinuous(callback);
+
+            String scriptForBlur = "document.getElementsByTagName('input')[0].blur();";
+
+            //String script = "var inputEle = document.getElementsByTagName('input')[0];var event1 = document.createEvent('HTMLEvents');  event1.initEvent('change', true, true); event1.eventType = 'change'; inputEle.dispatchEvent(event1);";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                browserTest.evaluateJavascript(scriptForBlur, null);
+            } else {
+                browserTest.loadUrl("javascript:" + scriptForBlur);
+            }
+
+        }
+    }//end else
+
+        //For menu action
+        public void torchOnOff_menu() {
+
+            isTorchOn = !isTorchOn;
+            if(isTorchOn == true) {
+                barcodeView.setTorchOn();
+            }else
+            {
+                barcodeView.setTorchOff();
+            }
+        }
+
 }
